9dea87df8315e861518d765192a9b10a08543cf8
Shubham Singh <imshubhamsingh97@gmail.com>
2017-11-22 16:51:14 +0530

    abstract function

--- myFirstContract.sol
+++ myFirstContract.sol
@@ -1,10 +1,32 @@
 pragma solidity ^0.4.8;
 
 contract Bank {
-    
+    // To proctect from outside world but want MyFirstContract to use it
+    // We use internal that is similar to protected
+    // uint internal myInternalValue;
+
+    uint private value;
+
+    function Bank(uint amount) {
+        value = amount;
+    }
+
+    function deposit(uint amount) {
+        value += amount;
+    }
+
+    function withdraw(uint amount) {
+        value += amount;        
+    }
+
+    function balance() returns (uint) {
+        return value;
+    }
+    //abstract function
+    //function loan() returns (bool);
 }
 
-contract MyFirstContract is Bank {
+contract MyFirstContract is Bank(10) {
     string private name;
     uint private age;
 
@@ -22,5 +44,9 @@ contract MyFirstContract is Bank {
 
     function getAge() returns (uint) {
         return age;                
+    }
+
+    function loan() returns (bool) {
+        return true;
     }
 }
