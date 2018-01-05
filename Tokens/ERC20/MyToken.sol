c8c842b3f11efde12e13acbe1bd517e69d028941
Shubham Singh <imshubhamsingh97@gmail.com>
2018-01-06 00:18:18 +0530

    finished ERC20 token

--- Tokens/ERC20/MyToken.sol
+++ Tokens/ERC20/MyToken.sol
@@ -10,7 +10,11 @@ contract MyFirstToken is ERC20 {
     uint private constant __totalSupply = 1000;
 
     mapping (address => uint) private __balanceOf;
-    mapping (address => mapping(address => uint)) private __allowance
+    mapping (address => mapping(address => uint)) private __allowances
+
+    function MyFirstToken(){
+        _balanceOf[msg.sender] = __totalSupply;
+    }
 
     function totalSupply() constant returns (uint _totalSupply){
         __totalSupply = __totalSupply;        
@@ -30,15 +34,24 @@ contract MyFirstToken is ERC20 {
      }
 
     function transferFrom(address _from, address _to, uint _value) returns (bool success){
-
+        if(__allowances[_from][msg.sender]> 0 &&
+           _value>0 && 
+           __allowances[_from][msg.sender] >= _value 
+        ){
+             __balanceOf[_from] -= _value;
+             __balanceOf[_to] += value;
+             return true;
+        }
+        return false;
     }
 
     function approve(address _spender, uint value) returns (bool success){
-
+        __allowances[msg.sender][_spender] = value
+        return true;
     }
 
     function allowance(address _owner, address _spender) constant returns (uint remaining){
-
+         return __allowances[_owner][_spender];
     }
 
 }
