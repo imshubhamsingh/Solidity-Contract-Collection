583b5eac8bae913fc87c6b8c7f05c015d75f7ce7
Shubham Singh <imshubhamsingh97@gmail.com>
2018-01-27 17:22:41 +0530

    util

---
+++ stringUtils.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.18;
+
+library StringUtils {
+
+    function compare(string _a, string _b) internal pure returns (int) {
+         bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint minLength = a.length;
+        if (b.length < minLength) minLength = b.length;
+        for (uint i = 0; i < minLength; i ++)
+            if (a[i] < b[i])
+                return -1;
+            else if (a[i] > b[i])
+                return 1;
+        if (a.length < b.length)
+            return -1;
+        else if (a.length > b.length)
+            return 1;
+        else
+            return 0;
+    }
+
+    function equal(string _a, string _b) internal pure returns (bool) {
+        return compare(_a, _b) == 0;
+    }
+
+     function indexOf(string _haystack, string _needle) internal pure returns (int)
+    {
+    	bytes memory h = bytes(_haystack);
+    	bytes memory n = bytes(_needle);
+    	if(h.length < 1 || n.length < 1 || (n.length > h.length)) 
+    		return -1;
+    	else if(h.length > (2**128 -1))
+           return -1;									
+    	else
+    	{
+    		uint subindex = 0;
+    		for (uint i = 0; i < h.length; i ++)
+    		{
+    			if (h[i] == n[0]) // found the first char of b
+    			{
+    				subindex = 1;
+    				while(subindex < n.length && (i + subindex) < h.length && h[i + subindex] == n[subindex]) // search until the chars don't match or until we reach the end of a or b
+    				{
+    					subindex++;
+    				}	
+    				if(subindex == n.length)
+    					return int(i);
+    			}
+    		}
+    		return -1;
+    	}	
+    }
+
+}
